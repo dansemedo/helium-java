d1d2b3aef48af4b52d37a2e9da765ceea9614959
Daniel Semedo <danpin@microsoft.com>
2019-06-24 10:58:45 -0300

    feat: Azure Key Vault

--- src/main/java/com/microsoft/azure/helium/Application.java
+++ src/main/java/com/microsoft/azure/helium/Application.java
@@ -1,13 +1,22 @@
 package com.microsoft.azure.helium;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner{
+	
+	@Value("${connectionString}")
+	private String connectionString;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	public void run(String... varl) throws Exception {
+		System.out.println(String.format("\nConnection String stored in Azure Key Vault:\n%s\n",connectionString));
+	 }
 
 }
